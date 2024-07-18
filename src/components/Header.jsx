47aefde76b227f0e373928cd3b65df32e13a20a2
Saqib <saqibsaifi720@.com>
2024-07-18 22:03:39 +0530

    morebug fixed in humbergur styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,21 +32,23 @@ const Header = () => {
     <div
       className={`fixed left-0 top-0 z-50 w-full border-b border-n-6 backdrop-blur-sm lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}
     >
-      <div className="xl:px-10 flex items-center px-5 max-lg:py-4 lg:px-7.5">
+      <div className="flex items-center px-5 max-lg:py-4 lg:px-7.5 xl:px-10">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           Saqib&rsquo;s Portfolio
         </a>
 
         <nav
-          className={`${openNavigation ? "flex" : "hidden"} fixed bottom-0 left-0 right-0 top-[5rem] bg-n-8 lg:static lg:mx-auto lg:flex lg:bg-transparent`}
+          className={`${openNavigation ? "flex" : "hidden"} fixed bottom-0 left-0 right-0 top-[4.5rem] bg-n-8 lg:static lg:mx-auto lg:flex lg:bg-transparent`}
         >
-          <div className={`relative ${openNavigation ? "w-full bg-black bg-opacity-90" : "bg-transparent"} m-auto flex flex-col items-center justify-center lg:flex-row bg-repeat-y`}>
+          <div
+            className={`relative ${openNavigation ? "w-full bg-black bg-opacity-90" : "bg-transparent"} m-auto flex flex-col items-center justify-center lg:flex-row`}
+          >
             {navigation.map((item) => (
               <a
                 key={item}
                 href={item.url}
                 onClick={handleClick}
-                className={`relative block font-code text-xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? "lg:hidden" : ""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-sm lg:font-semibold ${item.url === pathName.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"} lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                className={`relative block font-code text-xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? "lg:hidden" : ""} px-6 py-8 md:py-8 lg:-mr-0.25 lg:text-sm lg:font-semibold ${item.url === pathName.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"} lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
@@ -61,7 +63,11 @@ const Header = () => {
         >
           Message me
         </a>
-        <Button className="hidden lg:flex" href="https://github.com/Saqib-2002" target="_blank">
+        <Button
+          className="hidden lg:flex"
+          href="https://github.com/Saqib-2002"
+          target="_blank"
+        >
           GitHub
         </Button>
         <Button
